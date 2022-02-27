ce831b48a4e86936ce2f0918660676f21f45be68
ItalianComposer <movchan2001@bk.ru>
2022-02-27 13:13:54 +0300

    Добавлен скрипт таймера

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
         interval: 10000,
       });
     });
+// таймер обратного отсчета
+    document.addEventListener('DOMContentLoaded', function() {
+  const deadline = new Date(2022, 02, 26);
+  let timerId = timer;
+  function declensionNum(num, words) {
+    return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+  }
+
+  function countdownTimer() {
+    const diff = deadline - new Date();
+    if (diff <= 0) {
+      clearInterval(timerId);
+    }
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+    const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+    $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+    $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+    $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+  }
+
+  const $minutes = document.querySelector('.timer__minutes');
+  const $seconds = document.querySelector('.timer__seconds');
+  countdownTimer();
+  timerId = setInterval(countdownTimer, 1000);
+});
